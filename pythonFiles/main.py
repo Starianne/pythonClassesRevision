f56ae7aba384e4944c8328deeeaba090889b7d0f
Annelies <118388665+Starianne@users.noreply.github.com>
2026-01-14 12:10:26 +0000

    made getName and __str__ function

---
+++ pythonFiles/main.py
@@ -0,0 +1,18 @@
+class Prize():
+    def __init__(self, name, type, value):
+        self.name = name
+        self.type = type
+        self.value = value
+
+
+    def getName(self):
+        return self.name
+        #return self.__name
+    
+    def __str__(self):
+        return f"{self.name} has {self.value}"
+    
+A = Prize("Annelies","name",40)
+
+print(A.name)
+print(A)
