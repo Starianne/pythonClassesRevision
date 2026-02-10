e201fef8c2a4ee64e6e05ac99fbd62e66db31843
[Annelies S] <118388665+Starianne@users.noreply.github.com>
2026-02-10 23:22:32 +0000

    Changed to days, added inventory to goob and added turn off and turn on player movement

--- main.py
+++ main.py
@@ -7,16 +7,23 @@ from random import randint
 class Goob(pygame.sprite.Sprite):
     def __init__(self, scale):
         super().__init__()
-    
-        #spritesheet:
-        spritesheet_image = pygame.image.load('graphics/player/goob_spritesheet.png').convert_alpha() #getting sprite sheet
-        goob_spritesheet = spritesheet.SpriteSheet(spritesheet_image) #calling class
+
+        # spritesheet:
+        spritesheet_image = pygame.image.load('graphics/player/goob_spritesheet.png').convert_alpha()
+        goob_spritesheet = spritesheet.SpriteSheet(spritesheet_image)
         self.scale = scale
         self.goob_index = 0
+
+        # availability and invent
+        self.available = 1
+        self.money = 0
+        self.keys = 0
+        self.sweets = 0
+
         self.x_pos = 100
 
-        #goob frames
-        goob_stand = goob_spritesheet.get_image(0, 24, 24, scale) 
+        # goob frames
+        goob_stand = goob_spritesheet.get_image(0, 24, 24, scale)
         goob_step = goob_spritesheet.get_image(1, 24, 24, scale)
         goob_crouch = goob_spritesheet.get_image(2, 24, 24, scale)
         goob_jump = goob_spritesheet.get_image(3, 24, 24, scale)
@@ -34,10 +41,32 @@ class Goob(pygame.sprite.Sprite):
 
         self.goob_walk = [goob_stand, goob_step]
         self.goob_jumping = [goob_crouch, goob_jump]
-        
-        #set current image and rect
+
+        # set current image and rect
         self.image = self.goob_walk[0]
         self.rect = self.image.get_rect(topleft=(self.x_pos, 100))
+
+    # availability / inventory methods
+    def get_available(self):
+        return self.available
+
+    def add_money(self, money):
+        self.money = money
+
+    def add_keys(self, keys):
+        self.keys = keys
+
+    def add_sweets(self, sweets):
+        self.sweets = sweets
+
+    def get_money(self):
+        return self.money
+
+    def get_keys(self):
+        return self.keys
+
+    def get_sweets(self):
+        return self.sweets
 
     def animation_state(self, state):
         if state == "walk":
@@ -47,28 +76,30 @@ class Goob(pygame.sprite.Sprite):
             self.image = self.goob_walk[int(self.goob_index)]
 
     def player_input(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
-            self.animation_state("walk")
-            self.x_pos -= 6
-        elif keys[pygame.K_d]:
-            self.animation_state("walk")
-            self.x_pos += 6
-        self.rect.x = self.x_pos
-    
+        # only allow movement when available == 1
+        if self.available == 1:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_a]:
+                self.animation_state("walk")
+                self.x_pos -= 6
+            elif keys[pygame.K_d]:
+                self.animation_state("walk")
+                self.x_pos += 6
+            self.rect.x = self.x_pos
+
     def update(self):
         self.player_input()
 
 def generate():
-        container = ["an envelope", "a box", "a suitcase", "a chest"]
-        types = ["money", "deed","idk something"]
-        name = container[randint(0,3)]
-        type = types[randint(0,2)]
-        if type == "money":
-            value = 25 * randint(1, 15)
-        else:
-            value = randint(1,3)
-        return Prize(name, type, value)
+    container = ["an envelope", "a box", "a suitcase", "a chest"]
+    types = ["money", "sweet","key"]
+    name = container[randint(0,3)]
+    type = types[randint(0,2)]
+    if type == "money":
+        value = 25 * randint(1, 15)
+    else:
+        value = randint(1,3)
+    return Prize(name, type, value)
 
 class Prize(): #Prize class with 3 variables and their getters
     def __init__(self, name, type, value):
@@ -98,34 +129,10 @@ class Prize(): #Prize class with 3 variables and their getters
 
 
 
-def road_set(null_road): # sets 10 prizes randomly on the road set and gives the complete road back
-    flag = True
-    count = 10
-    
-    while flag == True:
-        if count != 0:
-            road_space = randint(0,49)
-            if null_road[road_space] == 0:
-                count -= 1
-                null_road[road_space] = generate()
-                print(road_space)
-        else:
-            flag = False
-    return null_road
-
-def display_road(road):
-    for i in range(0,49):
-        print(f"Space {i+1}")
-        if road[i] == 0:
-            print("empty")
-        else:
-            print(road[i].present())
-
-
-road = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-game_road = road_set(road)
-display_road(game_road)
-
+def day_events():
+    days = []
+    for i in range(0,99):
+        events = [ ]
 
 
 
@@ -164,9 +171,9 @@ tutorial = False
 tutorial_surface = pygame.Surface((int(screen_width/2), int(screen_height/2)))
 tutorial_surface.fill("white")
 
-#year:
-global current_year
-current_year = 0
+#day:
+global current_day
+current_day = 0
 
 
 #groups:
@@ -232,17 +239,25 @@ def tutorialFunc():
     tutorial = True
     return tutorial
 
-def incrementYearFunc():
-    global current_year
-    current_year += 1
-    return current_year
+def incrementDayFunc():
+    global current_day
+    current_day += 1
+    return current_day
+
+def turn_off_movement():
+    s = goob.sprite
+    if s is None:       #got annoyed and put this into chat, will test and comment on this tomorrow
+        return
+    s.available = 0 if s.available == 1 else 1
+    return s.available
+    
 #Button(30, 30, 400, 100, 'Button One (onePress)', myFunction)
 #Button (30, 140, 400, 100, 'Button Two(multiPress)', myFunction, True) hold down the button for multiple inputs
 
-Button(screen_width/2-(screen_width/10), screen_height/2, 400, 100, 'Start Game', startGameFunc)
-Button(screen_width/2-(screen_width/10), screen_height/2-(screen_height/10), 400, 100, 'Read Tutorial', tutorialFunc)
-Button(screen_width/2-(screen_width/10), screen_height/2+(screen_height/4), 400, 100, 'Keep Going!', incrementYearFunc) #press this and current year goes up
-
+Button(screen_width/2-(screen_width/10), screen_height/2-(screen_height/10), 400, 100, 'Start Game', startGameFunc)
+Button(screen_width/2-(screen_width/10), screen_height/2, 400, 100, 'Read Tutorial', tutorialFunc)
+Button(screen_width/2-(screen_width/10), screen_height/2+(screen_height/4), 400, 100, 'Keep Going!', incrementDayFunc) #press this and current day goes up
+Button(screen_width/2-(screen_width/10), screen_height/2, 400, 100, 'Turn on/off movement', turn_off_movement)
 
 
 while running:
@@ -264,21 +279,15 @@ while running:
 
     if game_active:
         screen.fill("#AFEBFA") 
-        year_surface = text_font.render("GOOB - Year "+ str(current_year), False, "black").convert_alpha()
+        year_surface = text_font.render("GOOB - Day "+ str(current_day), False, "black").convert_alpha()
         year_rect = year_surface.get_rect(center = (screen_width/2, (screen_height/2)-200))
         screen.blit(year_surface,year_rect)
         #Actual game goes in here
         
         objects[2].process() #next year button
+        objects[3].process() #turn off movement button to test disabling player input
         goob.draw(screen)
         goob.update()
-                  
-    
-
-
-
-
-
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000
