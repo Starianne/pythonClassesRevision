8dbf336079f35eabc4cfa21f57add70bfd58fb7f
[Annelies S] <118388665+Starianne@users.noreply.github.com>
2026-02-14 22:37:04 +0000

    Ok, commiting now bc i've lowkey realised I've messed up my game structure and maybe shouldn't have went into this with only the knowledge of how to put something on a screen and a collision? I'm going to have to re do a lot of my earlier stuff

--- holly_molly.py
+++ holly_molly.py
@@ -5,27 +5,76 @@ Holly = None
 Holly_rect = None
 Molly = None
 Molly_rect = None
-user = None
+Goob = None
 #event structure:
-choice = [["Winter", "Summer"]]
-event_dialogue = None
+#wait let me fix this:
+event_dialogue = {
+    "holly_start" : {
+        "character" : Holly,
+        "text" : "Hey, weird red thing",
+        "options" : [],
+        "next_states" : ["molly_start"]
+    },
+    "molly_start" : {
+        "character" : Molly,
+        "text" : "We need you to like, answer this question",
+        "options" : [],
+        "next_states" : ["holly_question"],
+    },
+        "holly_question" : {
+        "character" : Holly,
+        "text" : "Which season is better?",
+        "options" : [],
+        "next_states" : ["goob_answers"],
+    },
+        "goob_answers" : {
+        "character" : Goob,
+        "text" : "Uhhhhh",
+        "options" : ["Winter", "Summer"],
+        "next_states" : ["molly_agrees", "holly_agrees"],
+    },
+        "molly_agrees" : {
+        "character" : Molly,
+        "text" : "See Holly? Summer is for losers, you can literally just sit in a sunbed.",
+        "options" : [],
+        "next_states" : ["molly_rewards"],
+    },
+        "holly_agrees" : {
+        "character" : Holly,
+        "text" : "See Molly? I told you, Summer is the best.",
+        "options" : [],
+        "next_states" : ["holly_rewards"],
+    },
+        "molly_rewards" : {
+        "character" : Molly,
+        "text" : "Take this, since you weren't a moron",
+        "options" : [],
+        "next_states" : ["holly_grumbles"],
+    },
+        "holly_rewards" : {
+        "character" : Holly,
+        "text" : "Since you made the OBVIOUS correct choice",
+        "options" : [],
+        "next_states" : ["molly_grumbles"],
+    },
+        "molly_grumbles" : {
+        "character" : Molly,
+        "text" : "Go and crisp somewhere",
+        "options" : [],
+        "next_states" : ["goob_thanks"],
+    },
+        "holly_grumbles" : {
+        "character" : Holly,
+        "text" : "yeah whatever",
+        "options" : [],
+        "next_states" : ["goob_thanks"],
+    },
+        "goob_thanks" : {
+        "character" : Goob,
+        "text" : "thanks",
+        "options" : [],
+        "next_states" : [],
+    },
+}
+ 
 
-def holly_molly_event():
-    global Holly, Holly_rect, Molly, Molly_rect, event_dialogue
-    from main import screen, screen_width, screen_height, next_box #imports inside function to not have circular imports
-    
-    #now define all variables
-    Holly = pygame.Surface((int(screen_width/16), int(screen_height/8)))
-    Holly.fill("green")
-    Holly_rect = Holly.get_rect()
-    Molly = pygame.Surface((int(screen_width/16), int(screen_height/8)))
-    Molly.fill("red")
-    Molly_rect = Molly.get_rect()
-    
-    event_dialogue = [[Holly, "Hey, weird red thing"], [Molly, "Yeah, you come here"], [Holly, "which season is better"], [user, choice[0]]]
-    
-    i = 0
-    for dialogue in event_dialogue[i][1]:
-        surface_rect = event_dialogue[i][0].get_rect()
-        screen.blit(event_dialogue[i][0], surface_rect)
-        next_box(dialogue)
